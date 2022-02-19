2c06fa1ad55c548e1e7e0735d7063ed4f580e5be
Malkiel645 <71563100+Malkiel645@users.noreply.github.com>
2022-02-19 11:01:19 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+apiKey: "AIzaSyAMHjI8ClsFZi7c8zzClz3iFbEgEVXsrtM",
+  authDomain: "class-test-c955f.firebaseapp.com",
+  projectId: "class-test-c955f",
+  storageBucket: "class-test-c955f.appspot.com",
+  messagingSenderId: "381642525187",
+  appId: "1:381642525187:web:dc9c7749b6f27cb2029c36"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
